e4c40496e91388a1f9ec456de9cddee17e0e0a0a
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-03-17 13:51:37 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerComponent } from './components/player/player.component';
 
 // importamos los componentes para añadirlo a las rutas
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
 {path: 'home', component: HomeComponent},
+{path: 'player', component: PlayerComponent},
 {path: '**', pathMatch: 'full' ,redirectTo: 'home'}
 
 ];
