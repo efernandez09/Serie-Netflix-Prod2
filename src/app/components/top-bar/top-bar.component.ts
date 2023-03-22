559ed1d2d50314d689ff109412021350dbf7b3ac
afloresca <afloresca@uoc.edu>
2023-03-22 23:15:10 +0100

    añadida navegación a reparto en top-bar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
+  onAnchorClick ( ) {
+    let x = document.querySelector("#actors");
+    if (x){
+        x.scrollIntoView();
+    }
+  }
 }
