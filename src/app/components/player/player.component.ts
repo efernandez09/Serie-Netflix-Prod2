f95c7debffc730fa2961976cd2f00382835cd827
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-03-23 18:05:17 +0000

    Update player.component.ts

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Actors } from 'src/app/models/actors.inteface';
+import { ActorService } from 'src/app/services/ActorService/actor.service';
 
 @Component({
   selector: 'app-player',
@@ -6,5 +12,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent {
+actor: Actors;
 
+  name = "Angular";
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
+  isPlay: boolean = false;
+  toggleVideo(event: any) {
+    this.videoplayer.nativeElement.play();
+  }
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
+
+  makeBig() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 560;
+  }
+
+  makeSmall() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 320;
+  }
+
+  makeNormal() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 420;
+  }
+
+  restart() {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime = 0;
+  }
+
+  constructor(
+    private route: ActivatedRoute, //Para extraer el id de la ruta que ha creado el componente
+    private actorService: ActorService, //Para mostrar el actor
+    private location: Location //Servicio para interactuar con el navegador y volver hacia a atrás
+  ){}
+
+  ngOnInit(): void {
+    this.getActor();
+  }
+
+  /*Método para obtener el id del actor y acceder a su detalle*/
+  getActor(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.actorService.getActor(+id!).subscribe(Actors => this.actor = Actors);
+  }
 }
