b88e8b06382d5511d9929b2f3e6c17dd0efbde50
EriCode09 <fernandezperezps@gmail.com>
2023-04-13 16:50:15 +0200

    Subida de datos Ok! (menos imagenes y videos))

--- src/app/components/add-actor/add-actor.component.ts
+++ src/app/components/add-actor/add-actor.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { Actors } from 'src/app/models/actors.inteface';
 import { HandleActorsService } from 'src/app/services/handleActors/handle-actors.service';
 import { Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
 declare var $: any;
 
 @Component({
@@ -14,23 +17,23 @@ export class AddActorComponent implements OnInit {
 
   formulario: FormGroup;
   modalAbierto = false;
+  random_id = Math.random();
+  url : string;
 
-  constructor( private actorHandle: HandleActorsService, private db: Firestore ) {}
+  constructor( private actorHandle: HandleActorsService, private storage: Firestore ) {}
 
   ngOnInit(): void{
     
     this.formulario = new FormGroup({
-      id: new FormControl(),
+      id: new FormControl(this.random_id),
       name: new FormControl(),
       short_description: new FormControl(),
-      image: new FormControl(),
+      image: new FormControl([]),
       video: new FormControl(),
-      details: new FormControl({
-        bornDate: '',
-        nationality: '',
-        long_description: '',
-        hobbies: ''
-      })
+      bornDate: new FormControl(),
+      nationality: new FormControl(),
+      long_description: new FormControl(),
+      hobbies: new FormControl()
     })
 
   }
@@ -44,29 +47,35 @@ export class AddActorComponent implements OnInit {
     $('.modal').modal('hide');
   }
 
-  // Variable para ir añadiendo Actores manualmente (Cambiar el addActor(actores) del metodo onSubmit()).
-  actores: Actors = {
-    id: 1,
-    name: "Ursula Corberó",
-    short_description: "Tokio es una de las miembros más importantes del grupo y es conocida por su habilidad con las armas y su valentía en situaciones peligrosas.",
-    image: "ursula-corbero.jpeg",
-    video: "UrsulayMiguel.mp4",
-    details: {
-    bornDate: "11/08/1989",
-    nationality: "Española",
-    long_description: "Úrsula Corberó es una actriz española nacida en Barcelona en 1989. Es conocida por su papel de Tokio en la exitosa serie de televisión española 'La Casa de Papel'. Corberó ha trabajado en cine y televisión desde que era joven y ha recibido varios premios por sus actuaciones, incluyendo el premio a Mejor Actriz en el Festival de Cine de Málaga. Además de su carrera actoral, Corberó ha trabajado como modelo y ha participado en campañas publicitarias para varias marcas de moda y belleza.",
-    hobbies: "viajar, leer y la música.",
+  // subirImagen() {
+  //   const archivo = this.formulario.controls['image'].value;
+  //   const ruta = `imagenes/${archivo.name}`;
+  //   const referencia = firebase.storage().ref(ruta);
+  //   referencia.put(archivo).then(() => {
+  //     referencia.getDownloadURL().then((url: any) => {
+  //       this.url = url;
+  //       this.firestore.collection('imagenes').add({ url: this.url });
+  //     });
+  //   });
+  // }
 
-    }
-  }
+  // Variable para ir añadiendo Actores manualmente (Cambiar el addActor(actores) del metodo onSubmit()).
+  // actores: Actors = {
+  //   id: 1,
+  //   name: "Ursula Corberó",
+  //   short_description: "Tokio es una de las miembros más importantes del grupo y es conocida por su habilidad con las armas y su valentía en situaciones peligrosas.",
+  //   image: "ursula-corbero.jpeg",
+  //   video: "UrsulayMiguel.mp4",
+  //   bornDate: "11/08/1989",
+  //   nationality: "Española",
+  //   long_description: "Úrsula Corberó es una actriz española nacida en Barcelona en 1989. Es conocida por su papel de Tokio en la exitosa serie de televisión española 'La Casa de Papel'. Corberó ha trabajado en cine y televisión desde que era joven y ha recibido varios premios por sus actuaciones, incluyendo el premio a Mejor Actriz en el Festival de Cine de Málaga. Además de su carrera actoral, Corberó ha trabajado como modelo y ha participado en campañas publicitarias para varias marcas de moda y belleza.",
+  //   hobbies: "viajar, leer y la música.",
+  // }
 
   async onSubmit() {
-    // Te has quedado aqui @Eric
-    // const datos = this.formulario.getRawValue();
-    // console.log(datos)
+    
     const res = await this.actorHandle.addActor(this.formulario.value)
     console.log(res)
-    console.log(this.formulario.controls['name'].value)
 
   }
  
--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Actors, ActorsDetails } from 'src/app/models/actors.inteface';
+import { Actors } from 'src/app/models/actors.inteface';
 
 @Component({
   selector: 'app-detail',
