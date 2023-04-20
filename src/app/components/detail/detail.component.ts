c73699a4708efa2b76b8bf26b977ee0793c58c2c
PatriGracia <116113594+PatriGracia@users.noreply.github.com>
2023-04-20 15:36:25 +0200

    Revert "Patri prod2"

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -2,11 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Actors } from 'src/app/models/actors.inteface';
-import { Firestore, doc, getDoc } from 'firebase/firestore';
-import { HandleActorsService } from 'src/app/services/handleActors/handle-actors.service';
-import { Observable, map, switchMap } from 'rxjs';
-import { getStorage, getDownloadURL, listAll, ref } from 'firebase/storage';
+import { Actors, ActorsDetails } from 'src/app/models/actors.inteface';
+import { ActorService } from 'src/app/services/ActorService/actor.service';
 
 @Component({
   selector: 'app-detail',
@@ -14,24 +11,22 @@ import { getStorage, getDownloadURL, listAll, ref } from 'firebase/storage';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit{
-  actor: Actors[];
-  a: Actors;
-  images: Array<string> = [];
-  
+  actor: Actors;
 
   constructor(
     private route: ActivatedRoute, //Para extraer el id de la ruta que ha creado el componente
-    private location: Location, //Servicio para interactuar con el navegador y volver hacia a atrás
-    private _actorService: HandleActorsService //Servicio donde se incluyen los métodos para interactuar con la bbdd  
+    private actorService: ActorService, //Para mostrar el actor
+    private location: Location //Servicio para interactuar con el navegador y volver hacia a atrás
   ){}
   
   ngOnInit(): void {
-    //Recogemos id de la URl para acceder a ese actor en la base de datos
-    const idActor = this.route.snapshot.paramMap.get('id');
-    
-    //Llamamos al método del servicio que devuelve un actor
-    this._actorService.getOneActor(idActor).subscribe(actors => { 
-      this.a = actors});
+    this.getActor();
+  }
+
+  /*Método para obtener el id del actor y acceder a su detalle*/
+  getActor(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.actorService.getActor(+id!).subscribe(Actors => this.actor = Actors);
   }
 
   /*Función para volver al componente actors*/
