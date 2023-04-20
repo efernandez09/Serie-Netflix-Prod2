4ab776094c833632d13c03fcc94ea51aec394c15
pquintanap <116024429+pquintanap@users.noreply.github.com>
2023-04-20 21:30:53 +0100

    Update player.component.ts

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -2,6 +2,12 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Firestore, doc, getDoc } from 'firebase/firestore';
+import { HandleActorsService } from 'src/app/services/handleActors/handle-actors.service';
+import { Observable, map, switchMap } from 'rxjs';
+import { getStorage, getDownloadURL, listAll, ref } from 'firebase/storage';
+
+
 
 import { Actors } from 'src/app/models/actors.inteface';
 
@@ -11,7 +17,32 @@ import { Actors } from 'src/app/models/actors.inteface';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent {
-actor: Actors;
+//actor: Actors;
+actor$: Observable<Actors>;
+actor: Actors[];
+a: Actors;
+images: Array<string> = [];
+videos : Array<string> = [];
+
+constructor(
+  private route: ActivatedRoute, //Para extraer el id de la ruta que ha creado el componente
+  private location: Location, //Servicio para interactuar con el navegador y volver hacia a atrás
+  private _actorService: HandleActorsService //Servicio donde se incluyen los métodos para interactuar con la bbdd  
+){}
+
+ngOnInit(): void {
+  const idActor = this.route.snapshot.paramMap.get('id')
+  
+  this._actorService.getOneActor(idActor).subscribe(actors => { 
+    console.log("Este es mi : ", actors);
+    this.a = actors});
+}
+
+
+goBack():void {
+  this.location.back();
+}
+
 
   name = "Angular";
   @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
@@ -41,23 +72,8 @@ actor: Actors;
   }
 
   restart() {
-    let video: any = document.getElementById("my_video_1");
-    video.currentTime = 0;
+    let myVideo: any = document.getElementById("my_video_1");
+    myVideo.currentTime = 0;
   }
 
-  constructor(
-    private route: ActivatedRoute, //Para extraer el id de la ruta que ha creado el componente
-    // private actorService: ActorService, //Para mostrar el actor
-    private location: Location //Servicio para interactuar con el navegador y volver hacia a atrás
-  ){}
-
-  ngOnInit(): void {
-    this.getActor();
-  }
-
-  /*Método para obtener el id del actor y acceder a su detalle*/
-  getActor(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    // this.actorService.getActor(+id!).subscribe(Actors => this.actor = Actors);
-  }
 }
