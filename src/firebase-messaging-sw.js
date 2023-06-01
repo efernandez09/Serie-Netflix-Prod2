ea388c165887ea19924b36758dc96966391aac3a
EriCode09 <efernandezperez0@uoc.edu>
2023-06-01 13:00:54 +0200

    Algunos cambios para ir añadiendo las notificaciones

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
-export const environment = {
-    production: false,
-  firebase: {
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging.js');
+
+firebase.initializeApp({
     projectId: 'prod2-serienetflix',
     appId: '1:8734869459:web:bdadd0530b79914ed15126',
     storageBucket: 'prod2-serienetflix.appspot.com',
@@ -8,6 +9,6 @@ export const environment = {
     apiKey: 'AIzaSyCo9Flk_rWy686Dpmj0BUqpWRxt4ILsDK0',
     authDomain: 'prod2-serienetflix.firebaseapp.com',
     messagingSenderId: '8734869459',
-    vapidKey: 'BLmioMLpOhr4XJmYPo3MwEx3c1tpvOh4cj0BO1hGMFGnUsK4wwvJiKWTHmWwx733u_TW6-9q_KKuci7RBUqeM_I'
-  }
-}
+   });
+   
+const messaging = firebase.messaging();
